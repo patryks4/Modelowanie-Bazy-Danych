e4062b68803013bf12d097140032ccec1fe56ed4
patrykash <patryksieczka4@gmail.com>
2021-05-29 11:48:22 +0200

    Add methods to initialize application run

---
+++ src/main/resources/static/js/App.js
@@ -0,0 +1,52 @@
+'use strict';
+window.onload = init;
+let isMoving = false;
+let addRow = false;
+let selectedEntity;
+let entitiesMap = new Map;
+function createSVGElement(name) {
+    return document.createElementNS("http://www.w3.org/2000/svg", name);
+}
+function init() {
+    let addRowButton= document.getElementById("addRow");
+    addRowButton.addEventListener("click" ,function (){
+        selectedEntity.addRow();
+    });
+    let dragElements = document.querySelectorAll(".side-img");
+    dragElements.forEach(element => element.addEventListener("mousedown", switchOnMoving));
+    window.addEventListener("mousemove", moveImg);
+    window.addEventListener("mouseup", switchOffMoving);
+}
+
+function createEntity(event) {
+    let key = "entity-0";
+    let position = getMousePosition(event);
+    let entityNumber = 0;
+    while (entitiesMap.has(key)) {
+        entityNumber++;
+        key = "entity-" + entityNumber;
+    }
+    let entityView = new EntityView(position.x, position.y, key);
+    let entityBody = entityView.createBody();
+    document.getElementById("svg-view").appendChild(entityBody);
+    makeDraggable(entityBody);
+    entitiesMap.set(key, entityView);
+}
+
+function createRelation(event) {
+    let position = getMousePosition(event);
+
+    let relationView = new RelationView(position.x, position.y, "relation-0");
+    let relation = relationView.createBody();
+    document.getElementById("svg-view").appendChild(relation);
+    makeDraggable(relation);
+}
+
+
+
+
+
+
+
+
+
